5b2f052b691f40696ab6d8d07ce10068cdea368f
gamoorim <gabrielrock012@gmail.com>
2026-04-06 20:26:08 -0300

    upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,8 @@ const app = require('./app')
 
 require('dotenv').config()
 
-const port = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`Server rodando na porta ${port}`)
+    console.log(`Server rodando na porta ${PORT}`)
 })
