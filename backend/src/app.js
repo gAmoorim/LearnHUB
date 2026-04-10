3baae4d3571ff29cc867f07719876fe9d2a18c1f
gamoorim <gabrielrock012@gmail.com>
2026-04-10 17:17:09 -0300

    att cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,12 @@ const routers = require('./routers/routers')
 
 const app = express()
 
+app.use(cors({
+  origin: ['https://yourlearnhub.vercel.app', 'http://localhost:3000'],
+  credentials: false
+}))
+
 app.use(express.json())
-app.use(cors())
 app.use(routers)
 
 module.exports = app
